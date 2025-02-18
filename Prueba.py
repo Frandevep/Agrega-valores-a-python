d841030b301dad987e65577e31d1542d1366af5d
Frandevep <frandevep@gmail.com>
2025-02-18 19:29:02 -0300

    Add files via upload

---
+++ Prueba.py
@@ -0,0 +1,9 @@
+# Pedimos dos números al usuario
+num1 = float(input("Ingresa el primer número: "))
+num2 = float(input("Ingresa el segundo número: "))
+
+# Realizamos la suma
+resultado = num1 + num2
+
+# Mostramos el resultado
+print("La suma de", num1, "y", num2, "es:", resultado)
